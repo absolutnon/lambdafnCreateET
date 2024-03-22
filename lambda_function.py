8a6cfb97358e2cfd3a3f5de6e6b6a76b720dd69f
Pahuton Sriwichai <110499159+absolutnon@users.noreply.github.com>
2024-03-22 11:29:56 +0000

    Update lambda_function.py
    Added Object count function
    Added rename function

--- lambda_function.py
+++ lambda_function.py
@@ -3,11 +3,18 @@ import json
 import urllib.parse
 import os
 import boto3
+from time import gmtime, strftime
+import botocore
 
+# Initialise values
 PIPELINE_ID = os.environ['PIPELINE_ID']
-region_name = '[region of your Elastic Transcoder PIPELINE]'
+region_name = 'eu-west-1'
 transcoder = boto3.client('elastictranscoder', region_name=region_name)
 s3 = boto3.resource('s3')
+time = ['0630','0656','0730','0756','0830','0856','0930','0956','1030','1230','1330','1430', '1530', '1556', '1630', '1656', '1730', '1756', '1830', '1856']
+today_date = strftime('%d%m')
+today_folder = strftime('%d%m%y')
+file_run = int(os.environ['FILERUN'])
 
 def lambda_handler(event, context):
     print(event)
@@ -19,21 +26,27 @@ def lambda_handler(event, context):
     bucket_name = event['Records'][0]['s3']['bucket']['name']
     input_key = event['Records'][0]['s3']['object']['key']
     
+    output_name = rename_file(input_key)
+    
+    print(output_name)
+    # Check what is the input file name
     print(input_key)
     
     # Define the output file prefix (output will be saved in the same bucket)
-    output_key_prefix = 'output/'
+    output_key_prefix = 'to-mha/'
 
-    # Define the output file name and key, with folder in the bucket as prefix
-    output_key = output_key_prefix + input_key.rsplit('.', 1)[0] + '.mxf'
+    # Define the output file name and key
+    #output_key = output_key_prefix + input_key.rsplit('.', 1)[0] + '.mxf'
+    output_key = output_key_prefix + output_name + '.mxf'
     
+    # Check what is the output file name
     print(output_key)
 
     # Define the Elastic Transcoder pipeline ID
-    pipeline_id = '[PIPELINE_ID]'
+    pipeline_id = '1710868867557-60hfjd'
 
     # Define the preset ID for 1080i50 XDCAM422
-    preset_id = '[PRESET_ID]'
+    preset_id = '1351620000001-100230'
 
     # Define the output settings
     output_settings = [
@@ -62,3 +75,52 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': 'Transcoding job initiated successfully.'
     }
+    
+def rename_file(file_input_name):
+    bucket_name = 'gbn-gtn'
+    folder_path = 'tmp/' + today_folder
+    
+    file_name_no_extension = file_input_name.rsplit('.', 1)[0]
+    extracted_date = file_name_no_extension.rsplit('-', 1)[1]
+    year = extracted_date[:4]
+    month = extracted_date[4:6]
+    day = extracted_date[6:8]
+    hour = extracted_date[8:10]
+    min = extracted_date[10:12]
+    file_date = day + month
+    global output_name
+    global file_run
+    
+    # name_index = write_file_index(file_run)
+    
+    name_index = check_object()
+    
+    if today_date == file_date and file_run == 0:
+        output_name = 'INRIX_TRAFFIC_' + time[name_index - 1] + '_' + day + month + year
+        name_index = name_index + 1
+    else:
+        output_name = 'INRIX_TRAFFIC_' + time[name_index - 1] + '_' + day + month + year
+        name_index = name_index + 1
+    
+    
+    
+    return output_name
+    
+def check_object():
+    # connect to s3 - assuming your creds are all set up and you have boto3 installed
+    s3 = boto3.resource('s3')
+    
+    # identify the bucket - you can use prefix if you know what your bucket name starts with
+    for bucket in s3.buckets.all():
+        print(bucket.name)
+    
+    # get the bucket
+    bucket = s3.Bucket('gbn-gtn')
+    
+    # use loop and count increment
+    count_obj = 0
+    for i in bucket.objects.all():
+        count_obj = count_obj + 1
+    print(count_obj)
+    
+    return count_obj
